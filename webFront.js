7c767a30d6ac5bb58b52af56d951dd0cdc40efd8
potato <zhouzhengfa1995@foxmail.com>
2023-03-05 18:57:55 +0800

    as

---
+++ webFront.js
@@ -0,0 +1,26 @@
+//Bootstrap
+//Angular.js
+//React.js
+//Vue.js
+
+vx small program [EXP]
+
+MVVM framework.
+
+gulp ' webpack
+
+node.js
+
+Express/Koa2/Egg
+
+ES6/7
+
+browser compatible.
+
+css pretreatment . Sass.
+
+webFront unit test: Jest & Cypress
+
+
+element-ui . mint-ui . vant ..
+
